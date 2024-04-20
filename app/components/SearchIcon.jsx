daa9037f98fbd26a6c7c7f77c3ed43a44b1d941b
anonymous-sys19 <estudiosrg5@gmail.com>
2024-04-19 21:08:39 -0600

    Ones Components

--- app/components/SearchIcon.jsx
+++ app/components/SearchIcon.jsx
@@ -1,22 +1,22 @@
-export const InputComponentSearch = ({ placeholder, clasName}) => {
-  return (
+export const InputComponentSearch = ({ placeholder, clasName }) => {
+    return (
 
-      <input
-          type="text"
-          placeholder={placeholder}
-          className={clasName}
-          style={
-              {
-                  borderRadius: "0.375rem",
-                  width: "100%",
-                  background: "border-box",
-                  padding: "0.5rem 1rem",
-                  outline: "none",
-                  borderColor: "#4299e1",
-              }
-              
-          }
-      />
+        <input
+            type="text"
+            placeholder={placeholder}
+            className={clasName}
+            style={
+                {
+                    borderRadius: "0.375rem",
+                    width: "100%",
+                    background: "border-box",
+                    padding: "0.5rem 1rem",
+                    outline: "none",
+                    borderColor: "#4299e1",
+                }
 
-  )
+            }
+        />
+
+    )
 }
--- app/pages/QuienesSomos/page.jsx
+++ app/pages/QuienesSomos/page.jsx
@@ -5,12 +5,12 @@ function Page() {
     <>
       <HistoriaIDEC />
 
-      <section>
-        <div className="container">
-          Historia IDEC
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis optio veniam, tenetur ullam recusandae voluptates ipsam qui asperiores temporibus iusto quibusdam culpa corrupti iste libero dicta eaque quasi a fuga sit! Itaque repellendus quidem minima praesentium, esse cum omnis in! Impedit unde voluptatum fugiat assumenda sequi deserunt maiores necessitatibus, nihil voluptatem veritatis, iusto harum!
-        </div>
-      </section>
+
+      <div className="container">
+        Historia IDEC
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore omnis optio veniam, tenetur ullam recusandae voluptates ipsam qui asperiores temporibus iusto quibusdam culpa corrupti iste libero dicta eaque quasi a fuga sit! Itaque repellendus quidem minima praesentium, esse cum omnis in! Impedit unde voluptatum fugiat assumenda sequi deserunt maiores necessitatibus, nihil voluptatem veritatis, iusto harum!
+      </div>
+
     </>
   )
 }
