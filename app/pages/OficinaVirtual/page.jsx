bd535261de8844e10f4b437dbd672e2a3d1484af
anonymous-sys19 <estudiosrg5@gmail.com>
2024-04-19 18:29:40 -0600

    new camgios

---
+++ app/pages/OficinaVirtual/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OficinaVirtual() {
+  return (
+    <div>
+      Oficina Virtual
+    </div>
+  )
+}
+
+export default OficinaVirtual
--- app/pages/QuienesSomos/page.jsx
+++ app/pages/QuienesSomos/page.jsx
@@ -6,8 +6,8 @@ function Page() {
  
   return (
     <>
-      <HomeApp RutaDisplay={<HistoriaIDEC />} />
       
+      <HistoriaIDEC></HistoriaIDEC>
     </>
   )
 }
