be5fc62c97b81ac33466a780b74761ef3b99c679
greivin mayorga <estudiosrg5@gmail.com>
2024-06-04 01:55:26 -0600

    Modifi

--- app/pages/QuienesSomos/HomeQuienesSomos.jsx
+++ app/pages/QuienesSomos/HomeQuienesSomos.jsx
@@ -1,12 +1,13 @@
 'use client'
-
 import HomeMenuLinks from "../../components/HomesLinks/HomeMenuLinks";
 function HistoriaIDEC() {
+  function HandleClick ()  {
+    alert('{"Mision y Vision} {Principios Doctrinales} {Estructura } Se encuentran en Desarrollo');
+  };
 
 
   return (
     <>
-
       <HomeMenuLinks
         item1={"Home"} ref1={'/'}
         item2={"Conexion 20/30"} ref2={"QuienesSomos/conexion20-30"}
@@ -14,12 +15,14 @@ function HistoriaIDEC() {
         item4={"Declaracion de fe"} ref4={"QuienesSomos/declaracion-de-fe"}
         item5={"Principios Doctrinales"} ref5={"#"}
         item6={"Principios Practicos"} ref6={"QuienesSomos/principiosPracticos"}
-        item7={"Estructura"} ref7={"#"}
+        item7={"Estructura"} ref7={HandleClick()}
       />
 
     </>
   )
 }
+
+
 
 export default HistoriaIDEC;
 
